d8fe52b304a9e09ee973dfc585acc957443bfcb3
AlterVoltrix <TAHK250XL@mail.ru>
2025-08-11 01:59:15 +0300

    TelegramBot

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,45 +1,31 @@
-from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
-                          InlineKeyboardMarkup, InlineKeyboardButton)
+﻿from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
 
-# Главное меню
-main = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='Типичные вопросы FAQ')],
-        [KeyboardButton(text='Руководства'), KeyboardButton(text='Об устройствах')]
-    ],
-    resize_keyboard=True,
-    input_field_placeholder='Выберите раздел...'
-)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-# Кнопка "Назад" для инлайн-клавиатур
-back_button = InlineKeyboardButton(text="⬅ Назад", callback_data="back_to_main")
+from app.database.requests import get_categories, get_category_item
 
-# Меню FAQ
-catalog = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Как работать с Ultrime?', callback_data='WordUltrime')],
-    [InlineKeyboardButton(text='Можно ли прошивать 2 устройства?', callback_data='TwoDevices')],
-    [InlineKeyboardButton(text='Прошивка сбилась на полпути', callback_data='Error')],
-    [back_button]
-])
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
+                                     [KeyboardButton(text='Корзина')],
+                                     [KeyboardButton(text='Контакты'),
+                                      KeyboardButton(text='О нас')]],
+                           resize_keyboard=True,
+                           input_field_placeholder='Выберите пункт меню...')
 
-# Меню "Руководства" (шаблон для ссылок)
-guides = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Инструкция по работе с Ultrime', url='https://vostok.b4com.tech/Products/Files/DocEditor.aspx?fileid=72017')],  # Замените ссылку
-    [InlineKeyboardButton(text='Обновления', url='https://vostok.b4com.tech/Products/Files/DocEditor.aspx?fileid=89589')],
-    [InlineKeyboardButton(text='Тестирование', url='https://vostok.b4com.tech/Products/Files/DocEditor.aspx?fileid=96329')],
-    [InlineKeyboardButton(text='Документация', url='https://vostok.b4com.tech/Products/Files/DocEditor.aspx?fileid=65489')],
 
-    [back_button]
-])
+async def categories():
+    all_categories = await get_categories()
+    keyboard = InlineKeyboardBuilder()
+    for category in all_categories:
+        keyboard.add(InlineKeyboardButton(text=category.name, callback_data=f"category_{category.id}"))
+    keyboard.add(InlineKeyboardButton(text='На главную', callback_data='to_main'))
+    return keyboard.adjust(2).as_markup()
 
-# Меню "Об устройствах" (модели)
-devices = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='MRXXX', callback_data='device_mrxxx')],
-    [InlineKeyboardButton(text='CS21XX', callback_data='device_cs21xx')],
-    [InlineKeyboardButton(text='CS41XX', callback_data='device_cs41xx')],
-    [InlineKeyboardButton(text='CS61XX', callback_data='device_cs61xx')],
-    [InlineKeyboardButton(text='CS62XX', callback_data='device_cs62xx')],
-    [InlineKeyboardButton(text='CS63XX', callback_data='device_cs63xx')],
-    [InlineKeyboardButton(text='CS71XX', callback_data='device_cs71xx')],
-    [back_button]
-])
+
+async def items(category_id):
+    all_items = await get_category_item(category_id)
+    keyboard = InlineKeyboardBuilder()
+    for item in all_items:
+        keyboard.add(InlineKeyboardButton(text=item.name, callback_data=f"item_{item.id}"))
+    keyboard.add(InlineKeyboardButton(text='На главную', callback_data='to_main'))
+    return keyboard.adjust(2).as_markup()
--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
-
-
-
+from app.database.models import async_main
 
 
 
 async def main():
-    bot = Bot(token='8042096367:AAGGesB3G9-T5aNIuzyOMDewphjwXYct0Qc')
+    await async_main()
+    bot = Bot(token='8042096367:AAE_-GmSVq3I-tqOeWhtH1KqtBc9UJ_UJPM')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
+
 if __name__ == '__main__':
- try:
-    asyncio.run(main())
- except KeyboardInterrupt:
-    print('Бот выключен')
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
 
 
